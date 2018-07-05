38c60f57ae85a6522302f8f69768a087c9809b2f
NinjawarriorMP <32735104+NinjawarriorMP@users.noreply.github.com>
2018-07-05 12:13:59 -0400

    added basic app components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {FormsModule} from '@angular/forms';
 import {AngularFontAwesomeModule} from 'angular-font-awesome';
+import { HeaderComponent } from './header/header.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { UserProfileDashboardComponent } from './user-profile-dashboard/user-profile-dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LoginComponent,
+    RegisterComponent,
+    UserProfileDashboardComponent
   ],
   imports: [
     BrowserModule,
