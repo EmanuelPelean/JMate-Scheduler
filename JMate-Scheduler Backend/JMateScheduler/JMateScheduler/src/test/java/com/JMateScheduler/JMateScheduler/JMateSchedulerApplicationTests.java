1ee78a28ea5b3b82592e8fae305e1442479220bf
NinjawarriorMP <32735104+NinjawarriorMP@users.noreply.github.com>
2018-09-26 15:54:31 -0400

    inital commit of backend

---
+++ JMate-Scheduler Backend/JMateScheduler/JMateScheduler/src/test/java/com/JMateScheduler/JMateScheduler/JMateSchedulerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.JMateScheduler.JMateScheduler;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JMateSchedulerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
