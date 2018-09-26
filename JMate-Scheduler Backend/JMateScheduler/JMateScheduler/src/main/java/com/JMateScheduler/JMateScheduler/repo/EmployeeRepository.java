60cf5eb09e5cad49c69e0e5a28ca788ef6ddd25f
NinjawarriorMP <32735104+NinjawarriorMP@users.noreply.github.com>
2018-09-26 19:23:48 -0400

    created employee CrudRepository

--- JMate-Scheduler Backend/JMateScheduler/JMateScheduler/src/main/java/com/JMateScheduler/JMateScheduler/repo/EmployeeRepository.java
+++ JMate-Scheduler Backend/JMateScheduler/JMateScheduler/src/main/java/com/JMateScheduler/JMateScheduler/repo/EmployeeRepository.java
@@ -3,10 +3,17 @@
  */
 package com.JMateScheduler.JMateScheduler.repo;
 
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.JMateScheduler.JMateScheduler.model.Employee;
+
 /**
  * @author Manu
  *
  */
-public interface EmployeeRepository {
-
+public interface EmployeeRepository extends CrudRepository<Employee, Long> {
+	List<Employee> findByLastName(String name);
 }
+
